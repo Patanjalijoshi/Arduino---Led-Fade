7898cda7575f533110c04f4cc8284508fc200269
Patanjali <joshipattu12@gmail.com>
2022-08-26 17:20:56 +0530

    Create Led_fade_code.c.ino

---
+++ Led_fade_code.c/Led_fade_code.c.ino
@@ -0,0 +1,20 @@
+// led fade
+#define ledPin 2
+int brightness = 0; 
+int fadevalue = 2;
+
+void setup() {
+ pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() {
+ analogWrite(ledPin, brightness);
+
+ brightness = brightness + fadevalue;
+
+ if(brightness<=0 || brightness>=225){
+  fadevalue = -fadevalue;
+ }  
+ delay(20);
+}
